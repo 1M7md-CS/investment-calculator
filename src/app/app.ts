ab8d54e32e237280c68649401da3b854bb0c47e9
1M7md-CS <mohammadhammad464@gmail.com>
2026-03-12 01:05:06 +0300

    refactor: move investment calculation to service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { UserInput } from './user-input/user-input';
-import { type InvestmentInputs } from './investment-input.model';
 import { InvestmentResults } from './investment-results/investment-results';
-import { type Results } from './investment-results/results.model';
 
 @Component({
   selector: 'app-root',
@@ -11,29 +9,4 @@ import { type Results } from './investment-results/results.model';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  resultsData = signal<Results>([]);
-
-  onCalculateInvestmentResults(data: InvestmentInputs) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData.set(annualData);
-  }
-}
+export class App {}
--- src/app/user-input/user-input.ts
+++ src/app/user-input/user-input.ts
@@ -1,6 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { type InvestmentInputs } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -14,10 +14,9 @@ export class UserInput {
   enteredExpectedReturn = signal('');
   enteredDuration = signal('');
 
-  calculate = output<InvestmentInputs>();
-
+  private investmentService = inject(InvestmentService);
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       annualInvestment: +this.enteredAnnualInvestment(),
       expectedReturn: +this.enteredExpectedReturn(),
